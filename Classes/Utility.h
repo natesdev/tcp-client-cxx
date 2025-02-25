2f49a11edb1f6e5a1d9df0e95b204ccd7d241052
Natesworks <contact@natesworks.com>
2025-02-25 15:14:38 +0100

    remove showdown specific stuff

--- Classes/Utility.h
+++ Classes/Utility.h
@@ -10,10 +10,5 @@
 class Utility
 {
 public:
-    static void parseFields(const std::map<std::string, std::string> &fields);
     static void dumpPacket(const std::vector<uint8_t> &buffer, int packetID);
-    static bool isServerToClient(int messageType);
-    static std::vector<uint8_t> getBytes(const std::string &str);
-    static int generateID();
-    static std::string generateToken();
 };
--- Classes/Wrappers/CallingInstance.h
+++ Classes/Wrappers/CallingInstance.h
@@ -2,18 +2,7 @@
 
 #include <string>
 
-#include "Player.h"
-
-struct SetPlayerNameData
-{
-    std::string name;
-};
-
 struct CallingInstance
 {
-    Player player;
     int socket;
-
-    // Packet specific data
-    SetPlayerNameData setPlayerNameData;
 };
