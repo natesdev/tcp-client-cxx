eb038ae3ba2ac5aff26bd5768e619270860845de
Natesworks <contact@natesworks.com>
2025-03-09 13:48:16 +0100

    Update Bytestream.h

--- Classes/Bytestream.h
+++ Classes/Bytestream.h
@@ -5,7 +5,7 @@
 #include <cstddef>
 #include <string>
 
-#include "Core/Logger.h"
+#include "Logger.h"
 
 extern Logger logger;
 
@@ -22,14 +22,12 @@ public:
     int readInt8();
     int readInt16();
     int readInt24();
-    int readIntLittleEndian();
+    int readIntLittleEndian(); // Same as Big Endian, but the bytes are reversed.
     long readLong();
+    long long readLongLong();
     int readShort();
     std::string readString();
-    int readVint();
     bool readBoolean();
-    long readDataReference();
-    long readVlong();
     std::vector<uint8_t> readBytes(size_t length);
     std::string readCompressedString();
     void writeInt8(int value);
@@ -42,13 +40,11 @@ public:
     void writeShort(int value);
     void writeString(const std::string &value);
     void writeStringReference(const std::string &value);
-    void writeVint(int value);
-    void writeVlong(int32_t high, int32_t low);
     void writeBoolean(bool value);
     void writeDataReference(int high = 0, int low = -1);
     void writeCompressedString(const std::string &data);
     void writeByte(uint8_t value);
     void writeBytes(const uint8_t value, size_t length);
     void writeHexa(const std::string &data, size_t length);
-    void writePacketHeader(int packetID, int packetVersion);
+    void writePacketHeader(int packetID);
 };
